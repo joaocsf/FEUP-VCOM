44d49b5296b4548e176d16ff9cfbb6c88e67981a
Manuel Gomes <magg.slb@gmail.com>
2018-10-22 01:17:13 +0100

    Added working example

--- HandDetection/test.py
+++ HandDetection/test.py
@@ -2,11 +2,13 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import pprint
+import math
 
 from math import sqrt
 
+
 def clustering(image):
-    Z = image.reshape((-1,3))
+    Z = image.reshape((-1, 3))
     Z = np.float32(Z)
 
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
@@ -19,6 +21,7 @@ def clustering(image):
 
     return res2
 
+
 # Change the calculation to somehow use the average position of the point
 def addPointToHand(handPoints, newPoint, minDistance=20):
     canAdd = True
@@ -30,7 +33,7 @@ def addPointToHand(handPoints, newPoint, minDistance=20):
         if distance < minDistance:
             canAdd = False
             break
-    
+
     if canAdd:
         handPoints.append(newPoint)
 
@@ -51,11 +54,14 @@ def calculateHandPoints(contours):
 
 
 def processImage(image):
-    image = cv.GaussianBlur(image, (5,5), 1)
-    #image = clustering(image)    
+    # Apply Gaussian blur
+    image = cv.GaussianBlur(image, (5, 5), 1)
+
+    # image = clustering(image)
+    # Convert to HSV color-space
     hsvImage = cv.cvtColor(image, cv.COLOR_RGB2HSV)
 
-    #Calculate the lower and upper HS values
+    # Calculate the lower and upper HS values
     minH = 90
     maxH = 120
 
@@ -65,41 +71,152 @@ def processImage(image):
     lower = (minH,minS, 0)
     upper = (maxH, maxS, 255)
 
-    #Mask HS Values
+    # Mask HS Values
     mask = cv.inRange(hsvImage, lower, upper)
 
-    #cv.erode(mask, (5,5))
+    # cv.erode(mask, (5,5))
     # cv.dilate(mask, (5,5))
 
-    #Find the contours of different hands
+    # Find the contours of different hands
     ret, contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     pp = pprint.pprint
     pprint.pformat(pprint.PrettyPrinter, indent=4, depth=20)
     pp(contours)
 
-    #Calculate points closest to a POI
+    # Calculate points closest to a POI
     hands, hull_list = calculateHandPoints(contours)
 
-    #Draw a point in each POI
+    # Draw a point in each POI
     for hand in hands:
         for point in hand:
             x,y = point[0]
-            cv.circle(image, (x,y), 5, (0,255,0), thickness=5)
- 
-    #Draw the original contours and their respective hulls
-    cv.drawContours(image, contours, -1, (255,0,0), 2)
-    cv.drawContours(image, hull_list, -1, (0,0,255), 2)
+            cv.circle(image, (x, y), 5, (0, 255, 0), thickness=5)
 
-    #Draw the convex hand shape using POIs
-    cv.drawContours(image, np.array(hands), -1, (0,255,255), 2)
+    # Draw the original contours and their respective hulls
+    cv.drawContours(image, contours, -1, (255, 0, 0), 2)
+    cv.drawContours(image, hull_list, -1, (0, 0, 255), 2)
 
-    #Show each mask used
+    # Draw the convex hand shape using POIs
+    cv.drawContours(image, np.array(hands), -1, (0, 255, 255), 2)
+
+    # Show each mask used
     cv.imshow("Hand", image)
     cv.imshow("HSV", hsvImage)
     cv.imshow("Mask", mask)
     return image
 
- 
+
+
+def processImage1(image):
+    # Apply Gaussian blur
+    image = cv.GaussianBlur(image, (3, 3), 0)
+
+    # Convert to HSV color-space
+    hsvImage = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+    # Calculate the lower and upper HS values
+    minH = 0
+    maxH = 30
+
+    minS = 0
+    maxS = 255
+
+    lower = (minH, minS, 0)
+    upper = (maxH, maxS, 255)
+
+    # Mask HS Values to create a binary image
+    mask = cv.inRange(hsvImage, lower, upper)
+
+    # Morphological transformations to filter
+    erode = cv.erode(mask, np.ones((5, 5)), iterations=1)
+    dilation = cv.dilate(erode, np.ones((5, 5)), iterations=1)
+
+    filtered = cv.GaussianBlur(dilation, (5, 5), 0)
+    ret, threshold = cv.threshold(filtered, 120, 255, 0)
+
+    # Find the contours
+    ret, contours, hierarchy = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+
+    # Find contour with maximum area
+    contour = max(contours, key=lambda x: cv.contourArea(x))
+
+    # Create bounding rectangle around the contour
+    x, y, w, h = cv.boundingRect(contour)
+    cv.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 0)
+
+    # Find convex hull
+    hull = cv.convexHull(contour)
+
+    # Draw contour
+    drawing = np.zeros(image.shape, np.uint8)
+    cv.drawContours(drawing, [contour], -1, (0, 255, ), 0)
+    cv.drawContours(drawing, [hull], -1, (0, 0, 255), 0)
+
+    # Find convexity defects
+    hull = cv.convexHull(contour, returnPoints=False)
+    defects = cv.convexityDefects(contour, hull)
+
+    count_defects = 0
+
+    for i in range(defects.shape[0]):
+        s, e, f, d = defects[i, 0]
+        start = tuple(contour[s][0])
+        end = tuple(contour[e][0])
+        far = tuple(contour[f][0])
+
+        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+        angle = (math.acos((b**2 + c**2 - a**2)/(2*b*c))*180)/3.14
+
+        # if angle > 90 draw a circle at the far point
+        if angle <= 90:
+            count_defects += 1
+            cv.circle(image, far, 1, [0, 0, 255], -1)
+
+        cv.line(image, start, end, [0, 255, 0], 2)
+
+    if count_defects == 0:  # Differentiate when is a 0 or 1
+        print(1)
+    elif count_defects == 1:
+        print(2)
+    elif count_defects == 2:
+        print(3)
+    elif count_defects == 3:
+        print(4)
+    elif count_defects == 4:
+        print(5)
+    else:
+        pass
+
+
+    """ret, contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    pp = pprint.pprint
+    pprint.pformat(pprint.PrettyPrinter, indent=4, depth=20)
+    pp(contours)
+
+    # Calculate points closest to a POI
+    hands, hull_list = calculateHandPoints(contours)
+
+    # Draw a point in each POI
+    for hand in hands:
+        for point in hand:
+            x,y = point[0]
+            cv.circle(image, (x, y), 5, (0, 255, 0), thickness=5)
+
+    # Draw the original contours and their respective hulls
+    cv.drawContours(image, contours, -1, (255, 0, 0), 2)
+    cv.drawContours(image, hull_list, -1, (0, 0, 255), 2)
+
+    # Draw the convex hand shape using POIs
+    cv.drawContours(image, np.array(hands), -1, (0, 255, 255), 2)
+"""
+    # Show each mask used
+    cv.imshow("Hand", image)
+    cv.imshow("HSV", hsvImage)
+    cv.imshow("Mask", mask)
+    cv.imshow("Thresholded", threshold)
+
 
 def test():
     # video = cv.VideoCapture(0)
@@ -110,10 +227,12 @@ def test():
     #     if(cv.waitKey(1) == 27):
     #         break
 
-    image = cv.imread('hands.jpg', cv.IMREAD_COLOR)
+    # Image read
+    image = cv.imread('../handsPosesDataSet/3/3.jpg', cv.IMREAD_COLOR)
 
-    image = cv.GaussianBlur(image, (5,5), 1)
-    result = processImage(image)
+    processImage1(image)
+
+    #result = processImage(image)
 
     # hsvImage = cv.cvtColor(result, cv.COLOR_RGB2HSV)
 
